21a144ac7a597588a1a0f924967f0740c1920395
Azad Kamarbandi <akamarbandi@unitedprint.rdb>
2024-04-05 23:23:18 +0200

    added calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCalculatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
